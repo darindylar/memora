ba6f0182b254871331829bd9aa84062196166e4c
darindylar <darindylar23@gmail.com>
2025-10-25 23:14:58 +0100

    add footer component to display user status and navigation links

---
+++ memora/components/footer.py
@@ -0,0 +1,25 @@
+import reflex as rx
+from memora.state.auth_state import AuthState
+
+def footer() -> rx.Component:
+    return rx.cond(
+        AuthState.current_username != "",
+        rx.hstack(
+            rx.text("memora", size="3", color="gray", weight="bold"),
+            rx.text("|", size="3", color="gray"),
+            rx.text(f"Logged in as: {AuthState.current_username.capitalize()}", size="3", color="gray"),
+            rx.spacer(),
+            rx.link("Status", href="/test", underline="hover"),
+            rx.link("Settings", href="/settings", underline="hover"),
+            width="100%",
+            padding_y="1em",
+        ),
+        rx.hstack(
+            rx.text("memora", size="3", color="gray", weight="bold"),
+            rx.spacer(),
+            rx.link("Status", href="/test", underline="hover"),
+            rx.link("Settings", href="/settings", underline="hover"),
+            width="100%",
+            padding_y="1em",
+        ),
+    )
