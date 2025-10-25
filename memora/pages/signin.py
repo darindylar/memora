d4885a75892b6f602254da6bdc06163535efac7b
darindylar <darindylar23@gmail.com>
2025-10-26 00:39:37 +0100

    refactor signin and signup forms to use on_click for submission and fix prod issue

--- memora/pages/signin.py
+++ memora/pages/signin.py
@@ -6,18 +6,34 @@ def signin() -> rx.Component:
     return rx.container(
         rx.vstack(
             rx.text("Sign in to your account", size="7", weight="bold"),
-            rx.form(
-                rx.vstack(
-                    rx.input(placeholder="Email", value=AuthState.email, on_change=AuthState.set_email, width="100%"),
-                    rx.input(placeholder="Password", type="password", value=AuthState.password, on_change=AuthState.set_password, width="100%"),
-                    rx.button("Sign in", type="submit", width="100%", loading=AuthState.loading),
-                    rx.cond(AuthState.error != "", rx.text(AuthState.error, color="red")),
-                    spacing="3", width="100%",
+            rx.vstack(
+                rx.input(
+                    placeholder="Email",
+                    value=AuthState.email,
+                    on_change=AuthState.set_email,
+                    width="100%",
                 ),
-                on_submit=AuthState.signin, reset_on_submit=False,
+                rx.input(
+                    placeholder="Password",
+                    type="password",
+                    value=AuthState.password,
+                    on_change=AuthState.set_password,
+                    width="100%",
+                ),
+                rx.button(
+                    "Sign in",
+                    width="100%",
+                    loading=AuthState.loading,
+                    on_click=AuthState.signin,          
+                ),
+                rx.cond(AuthState.error != "", rx.text(AuthState.error, color="red")),
+                spacing="3",
+                width="100%",
             ),
             footer(),
-            spacing="6", width="100%",
+            spacing="6",
+            width="100%",
         ),
-        size="4", py="28px",
+        size="4",
+        py="28px",
     )
--- memora/pages/signup.py
+++ memora/pages/signup.py
@@ -6,19 +6,40 @@ def signup() -> rx.Component:
     return rx.container(
         rx.vstack(
             rx.text("Create your memora account", size="7", weight="bold"),
-            rx.form(
-                rx.vstack(
-                    rx.input(placeholder="Username", value=AuthState.username, on_change=AuthState.set_username, width="100%"),
-                    rx.input(placeholder="Email", value=AuthState.email, on_change=AuthState.set_email, width="100%"),
-                    rx.input(placeholder="Password", type="password", value=AuthState.password, on_change=AuthState.set_password, width="100%"),
-                    rx.button("Sign up", type="submit", width="100%", loading=AuthState.loading),
-                    rx.cond(AuthState.error != "", rx.text(AuthState.error, color="red")),
-                    spacing="3", width="100%",
+            rx.vstack(
+                rx.input(
+                    placeholder="Username",
+                    value=AuthState.username,
+                    on_change=AuthState.set_username,
+                    width="100%",
                 ),
-                on_submit=AuthState.signup, reset_on_submit=False,
+                rx.input(
+                    placeholder="Email",
+                    value=AuthState.email,
+                    on_change=AuthState.set_email,
+                    width="100%",
+                ),
+                rx.input(
+                    placeholder="Password",
+                    type="password",
+                    value=AuthState.password,
+                    on_change=AuthState.set_password,
+                    width="100%",
+                ),
+                rx.button(
+                    "Sign up",
+                    width="100%",
+                    loading=AuthState.loading,
+                    on_click=AuthState.signup,         
+                ),
+                rx.cond(AuthState.error != "", rx.text(AuthState.error, color="red")),
+                spacing="3",
+                width="100%",
             ),
             footer(),
-            spacing="6", width="100%",
+            spacing="6",
+            width="100%",
         ),
-        size="4", py="28px",
+        size="4",
+        py="28px",
     )
